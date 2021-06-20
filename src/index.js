b3b4f262ef18d0d0c865b2666ea0f86e62e192c6
Vichovska <viictooriiaarch@gmail.com>
2021-06-19 20:49:13 -0500

    updated forecast js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function formatDate(timestamp) {
   return `${day} ${month} ${dates}, ${year} / ${hours}: ${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -61,12 +61,19 @@ function displayForecast() {
     </div>
   </div>
     `;
-   )}; 
+   }); 
    
    forecastHTML = forecastHTML + `</div>`;
    forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6ba2c825fb47daedbe3a55b4a9cb3ca8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+
+}
 function displayTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector("#temp");
@@ -88,6 +95,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
